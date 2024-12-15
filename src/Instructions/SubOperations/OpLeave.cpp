644c87ca60c69375a794dc8e87ad0fc446df0600
yubie-re <24496817+yubie-re@users.noreply.github.com>
2024-12-14 19:04:31 -0500

    document leave more

--- src/Instructions/SubOperations/OpLeave.cpp
+++ src/Instructions/SubOperations/OpLeave.cpp
@@ -28,12 +28,12 @@ bool OpLeave::GetInstructionLowLevelIL(const uint8_t* data, uint64_t addr, size_
     uint8_t returnSize = data[1];
 
     il.AddInstruction(il.SetRegister(4, Reg_R1, il.Add(4, il.Register(4, Reg_SP), il.Const(4, returnSize * 4)))); // return values
-    il.AddInstruction(il.SetRegister(4, Reg_SP, il.Register(4, Reg_FP)));
-    il.AddInstruction(il.SetRegister(4, Reg_FP, il.Pop(4)));
-    il.AddInstruction(il.SetRegister(4, Reg_R2, il.Pop(4)));
+    il.AddInstruction(il.SetRegister(4, Reg_SP, il.Register(4, Reg_FP))); // Get OG SP back
+    il.AddInstruction(il.SetRegister(4, Reg_FP, il.Pop(4))); // Restore OG FP
+    il.AddInstruction(il.SetRegister(4, Reg_R2, il.Pop(4))); // Get return address, store in R2
 
-    for(int i = 0; i < returnSize; i++)
-        il.AddInstruction(il.Push(4, il.Load(4, il.Sub(4, il.Register(4, Reg_R1), il.Const(4, 4 * returnSize)))));
+    for(int i = returnSize; i > 0; i--)
+        il.AddInstruction(il.Push(4, il.Load(4, il.Sub(4, il.Register(4, Reg_R1), il.Const(4, i * 4)))));
 
     if(il.GetFunction() && !il.GetFunction()->GetStart())
     {
